73e3665e67d6208bb8b9f43bb4231461a414e7b5
Luciano Silva <lsilva@pivotal.io>
2021-03-04 14:58:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.get('/', (request, response) => {
   response.send(`<!DOCTYPE html>
 <html>
   <head>
-    <title>Powered By Buildpacks</title>
+    <title>Powered By Cloud Native Buildpacks</title>
   </head>
   <body>
-    <h1>Node.js Application Container Image Powered by Cloud Native Buildpacks</h1>
+    <h1>Welcome! !! Node.js Application Container Image Powered by Cloud Native Buildpacks</h1>
     <h3>Hostname: ` + process.env.HOSTNAME + `</h3>
     <br/>
     <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://docs.vmware.com/en/VMware-Tanzu-Kubernetes-Grid/images/GUID-8546DDD9-718A-42F7-9EDB-0BCC3A316BB6-low.png"></img>
